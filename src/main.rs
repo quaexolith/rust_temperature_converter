5fb1464ddda5aa4426b6bf3eb8a3cc6002478007
quaexolith <quaexolith@gmail.com>
2022-07-09 13:11:56 -0400

    Convert Fahrenheit to Celcius and Celcius to Fahrenheit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
 fn main() {
-    println!("Hello, world!");
+    convert_fahrenheit_to_celcius();
+
+    convert_celcius_to_fahrenheit();
+}
+
+fn convert_fahrenheit_to_celcius() {
+    let fahrenheit_temp = 32.0;
+
+    let fahrenheit_temp_converted_to_celcius = convert_to_celcius(fahrenheit_temp);
+
+    println!("{fahrenheit_temp} F = {fahrenheit_temp_converted_to_celcius} C");
+}
+
+fn convert_to_celcius(fahrenheit_temp: f64) -> f64 {
+    println!("Converting Fahrenheit to Celcius");
+
+    (fahrenheit_temp - 32.0) / 1.8
+}
+
+fn convert_celcius_to_fahrenheit() {
+    let celcius_temp = 60.2;
+
+    let celcius_temp_converted_fahrenheit = convert_to_fahrenheit(celcius_temp);
+
+    println!("{celcius_temp} C = {celcius_temp_converted_fahrenheit} F");
+}
+
+fn convert_to_fahrenheit(celcius_temp: f64) -> f64 {
+    println!("Converting Celcius to Fahrenheit");
+
+    (celcius_temp * 1.8) + 32.0
 }
