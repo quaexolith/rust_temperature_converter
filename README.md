8929ea4814549007f51de0be279aab6421f85934
quaexolith <quaexolith@gmail.com>
2022-07-09 13:16:44 -0400

    Document temperature conversion calculations

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# rust_temperature_converter
-Rust Temperature Converter
+# Rust Temperature Converter
+A simple program in Rust demonstrating the conversion between:
+* Fahrenheit -> Celsius
+* Celsius -> Fahrenheit
+
+## Fahrenheit to Celsius
+```
+Celsius = (Fahrenheit - 32.0) * 1.8
+```
+
+## Celsius to Fahrenheit
+```
+Fahrenheit = (Celsius * 1.8) + 32.0
+```
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,33 @@
 fn main() {
-    convert_fahrenheit_to_celcius();
+    convert_fahrenheit_to_celsius();
 
-    convert_celcius_to_fahrenheit();
+    convert_celsius_to_fahrenheit();
 }
 
-fn convert_fahrenheit_to_celcius() {
+fn convert_fahrenheit_to_celsius() {
     let fahrenheit_temp = 32.0;
 
-    let fahrenheit_temp_converted_to_celcius = convert_to_celcius(fahrenheit_temp);
+    let fahrenheit_temp_converted_to_celsius = convert_to_celsius(fahrenheit_temp);
 
-    println!("{fahrenheit_temp} F = {fahrenheit_temp_converted_to_celcius} C");
+    println!("{fahrenheit_temp} F = {fahrenheit_temp_converted_to_celsius} C");
 }
 
-fn convert_to_celcius(fahrenheit_temp: f64) -> f64 {
-    println!("Converting Fahrenheit to Celcius");
+fn convert_to_celsius(fahrenheit_temp: f64) -> f64 {
+    println!("Converting Fahrenheit to Celsius");
 
     (fahrenheit_temp - 32.0) / 1.8
 }
 
-fn convert_celcius_to_fahrenheit() {
-    let celcius_temp = 60.2;
+fn convert_celsius_to_fahrenheit() {
+    let celsius_temp = 60.2;
 
-    let celcius_temp_converted_fahrenheit = convert_to_fahrenheit(celcius_temp);
+    let celsius_temp_converted_fahrenheit = convert_to_fahrenheit(celsius_temp);
 
-    println!("{celcius_temp} C = {celcius_temp_converted_fahrenheit} F");
+    println!("{celsius_temp} C = {celsius_temp_converted_fahrenheit} F");
 }
 
-fn convert_to_fahrenheit(celcius_temp: f64) -> f64 {
-    println!("Converting Celcius to Fahrenheit");
+fn convert_to_fahrenheit(celsius_temp: f64) -> f64 {
+    println!("Converting Celsius to Fahrenheit");
 
-    (celcius_temp * 1.8) + 32.0
+    (celsius_temp * 1.8) + 32.0
 }
